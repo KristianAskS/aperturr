b3437d618adcd878e1f8f685450d70318f2f3230
KristianAskS <kristians.sin.konto@gmail.com>
2025-03-31 01:14:49 +0200

    feat(Layout): Added opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,23 @@ import {
 
 export const metadata: Metadata = {
   title: "Aperturr",
-  description: "Aperturrbotsystemet.",
+  description: "Aperturrbotsystemet",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    type: "website",
+    url: "https://aperturr.no",
+    title: "Aperturr",
+    description: "Aperturrbotsystemet",
+    images: [
+      {
+        url: "https://images.steamusercontent.com/ugc/947329247059594679/7452EDDCDDA67C21F8558191E42CD4E07187988B/?imw=512&imh=512&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+        width: 512,
+        height: 512,
+        alt: "Aperturr",
+      },
+    ],
+    siteName: "Aperturr",
+  },
 };
 
 export default function RootLayout({
